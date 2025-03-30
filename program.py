10a2c6b156ede5e91cbcd71d32a028e41fc81a2c
Marito <maritoespana@gmail.com>
2025-03-29 18:48:38 -0700

    added python module for newer python compatibility, and changed user agent to be more specific

--- program.py
+++ program.py
@@ -70,8 +70,7 @@ def mainone(searchParam):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
-
+    options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
 
     driver=uc.Chrome(options=options)
     # target_driver = uc.Chrome(options=options)
